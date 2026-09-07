d94d01b7852b851d7a85df5614995c793a85b638
Mohith R Gowda <eng25am0073@dsu.edu.in>
2026-09-07 23:49:20 +0530

    Update AddDigits.java

--- AddDigits.java
+++ AddDigits.java
@@ -3,7 +3,6 @@ public class AddDigits {
         if (num == 0) return 0;
         return 1 + (num - 1) % 9;  // digital root formula
     }
-
     public static void main(String[] args) {
         int num = 38;
         System.out.println(AddDigits(num)); // Output: 2
