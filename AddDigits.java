7daa84fe0c111657a9fceff7446810f39d585955
Mohith R Gowda <eng25am0073@dsu.edu.in>
2026-09-07 20:46:48 +0530

    a

---
+++ AddDigits.java
@@ -0,0 +1,13 @@
+public class AddDigits {
+    public static int AddDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;  // digital root formula
+    }
+
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(AddDigits(num)); // Output: 2
+    }
+}
+
+    
