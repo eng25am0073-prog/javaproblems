8701ee3f39be10086b4ca9f831235e78142332c3
Mohith R Gowda <eng25am0073@dsu.edu.in>
2026-09-07 23:50:03 +0530

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -2,14 +2,12 @@
 class Solution {
    Solution() {
    }
-
    public boolean isPalindrome(int var1) {
       if (var1 >= 0 && (var1 % 10 != 0 || var1 == 0)) {
          int var2;
          for(var2 = 0; var1 > var2; var1 /= 10) {
             var2 = var2 * 10 + var1 % 10;
          }
-
          return var1 == var2 || var1 == var2 / 10;
       } else {
          return false;
